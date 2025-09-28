5e2bb3fa30be47c3ea07acd8491d1fac7dcf6455
Paulo Kauê <paulokaue.sp@gmail.com>
2025-09-27 23:23:32 -0300

    :arrow_up: Estava importando uma tela desnecessária

--- lib/pages/agendamento_page.dart
+++ lib/pages/agendamento_page.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:caixa_flutter/pages/tela_inicial.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../widgets/app_drawer.dart';
